e4bac3b5da8c09c3cad262f1cfd23b955f268604
donkey3456 <donkey.3456@163.com>
2018-01-10 00:16:34 +0800

    add more function

--- LLogger.cpp
+++ LLogger.cpp
@@ -1,31 +1,141 @@
 #include "stdafx.h"
 #include "LLogger.h"
+#include <ctime>
 
 
 using namespace std;
 
-
-
-LLogger::LLogger(std::ostream & stream)
-	:_output_stream(stream),
-	_enable_info(false),_enable_err(true){}
-
-LLogger::~LLogger()
+namespace lu
 {
-}
+#if !defined PROJECT_NAME
+#define PROJECT_NAME ""
+#endif
 
-LLogger::PrintThread LLogger::Info()
-{
-	return PrintThread(_mutex_stream,_output_stream,"[Info]",_enable_info);
-}
+	LLogger lerr(std::cerr);
+	LLogger lout(std::cout);
+	ofstream logfile("record/"+string(PROJECT_NAME) + ".log", ios::app | ios::out);
+	LLogger llog(logfile);
 
-LLogger::PrintThread LLogger::Err()
-{
-	return PrintThread(_mutex_stream, _output_stream, "[Error]",_enable_err);
-}
+	LLogger::LLogger(std::ostream & stream)
+		:_output_stream(stream), _enable_info(false),
+		_enable_debug(false), _enable_err(true),
+		_old_buf(nullptr),_enable(true)
+	{}
 
-void LLogger::Redirect(std::ostream& output_stream)
-{
-	_output_stream.rdbuf(output_stream.rdbuf());
+	LLogger::~LLogger()
+	{
+		if (_output_file.is_open())
+		{
+			_output_file.close();
+		}
+	}
+
+	LLogger::PrintThread LLogger::Info()
+	{
+		return Output("Info", _enable_info);
+	}
+
+	LLogger::PrintThread LLogger::Err()
+	{
+		return Output("Err", _enable_err);
+	}
+
+	lu::LLogger::PrintThread LLogger::Debug()
+	{
+		return Output("Debug", _enable_debug);
+	}
+
+	lu::LLogger::PrintThread LLogger::Log(std::string title)
+	{
+		bool enable = true;
+
+		{
+			lock_guard<mutex> lock(_mutex_map);
+
+			auto iter = _enable_map.find(title);
+			if (iter != _enable_map.end())
+			{
+				enable = iter->second;
+			}
+			_enable_map[title] = true;
+		}
+
+		return Output(title, enable);
+	}
+
+	void LLogger::EnableLog(std::string title, bool enable)
+	{
+		lock_guard<mutex> lock(_mutex_map);
+		_enable_map[title] = enable;
+	}
+
+	void LLogger::AddLog(std::string str, bool enable /*= true*/)
+	{
+		lock_guard<mutex> lock(_mutex_map);
+		_enable_map.insert(make_pair(str,enable));
+	}
+
+	void LLogger::RemoveLog(std::string str)
+	{
+		lock_guard<mutex> lock(_mutex_map);
+		_enable_map.erase(str);
+	}
+
+	void LLogger::RemoveAllLog()
+	{
+		lock_guard<mutex> lock(_mutex_map);
+		_enable_map.clear();
+	}
+
+	void LLogger::RedirectToFile(std::string filename)
+	{
+		if (_output_file.is_open())
+		{
+			_output_file.close();
+		}
+		if (filename.find('.') == std::string::npos)
+		{
+			filename += ".log";
+		}
+		_output_file.open(filename, ios::app | ios::out);
+
+		Redirect(_output_file);
+	}
+
+	void LLogger::RedirecBack()
+	{
+		if (_old_buf)
+		{
+			_output_stream.rdbuf(_old_buf);
+			_old_buf = nullptr;
+		}
+	}
+
+	void LLogger::Redirect(std::ostream& output_stream)
+	{
+		if (_old_buf)
+		{
+			_output_stream.rdbuf(output_stream.rdbuf());
+		}
+		else
+		{
+			_old_buf = _output_stream.rdbuf(output_stream.rdbuf());
+		}
+	}
+
+	LLogger::PrintThread LLogger::Output(std::string title,bool enable)
+	{
+		time_t t = time(0);
+		tm now;
+		localtime_s(&now,&t);
+
+		char buffer[50];
+		int count = sprintf_s(buffer, 50, "[%04d-%02d-%02d %02d:%02d:%02d],",
+			now.tm_year + 1900, now.tm_mon + 1, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec);
+
+		string str_time(buffer);
+		return PrintThread(_mutex_stream, _output_stream, "[" + title + "]," + str_time, enable && _enable);
+	}
+
 }
 
--- LLogger.h
+++ LLogger.h
@@ -2,70 +2,94 @@
 
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <mutex>
+#include <map>
 
-class LLogger 
+namespace lu
 {
-	class PrintThread : public std::ostringstream
+	class LLogger
 	{
-	public:
-		PrintThread(std::mutex& mutex, std::ostream& stream, std::string prefix = "", bool verbose = true)
-			:_mutexPrint(mutex), _output_stream(stream), _prefix(prefix), _verbose(verbose) {}
-
-		PrintThread(const PrintThread& p)
-			:_mutexPrint(p._mutexPrint), _output_stream(p._output_stream),
-			_prefix(p._prefix), _verbose(p._verbose) {}
-
-		~PrintThread()
+		class PrintThread : public std::ostringstream
 		{
-			if (str().size() != 0 && _verbose)
-			{
-				std::lock_guard<std::mutex> guard(_mutexPrint);
-				_output_stream << _prefix <<str();
-				if (*str().rbegin() != '\n')
-				{
-					_output_stream << '\n';
-				}
-				_output_stream.flush();
-			}
-		}
+		public:
+			PrintThread(std::mutex& mutex, std::ostream& stream, std::string prefix = "", bool verbose = true)
+				:_mutexPrint(mutex), _output_stream(stream), _prefix(prefix), _verbose(verbose) {}
 
+			PrintThread(const PrintThread& p)
+				:_mutexPrint(p._mutexPrint), _output_stream(p._output_stream),
+				_prefix(p._prefix), _verbose(p._verbose) {}
+
+			~PrintThread()
+			{
+				if (str().size() != 0 && _verbose)
+				{
+					std::lock_guard<std::mutex> guard(_mutexPrint);
+					_output_stream << _prefix << str();
+					if (*str().rbegin() != '\n')
+					{
+						_output_stream << '\n';
+					}
+					_output_stream.flush();
+				}
+			}
+
+		protected:
+			std::mutex& _mutexPrint;
+			std::ostream& _output_stream;
+			std::string _prefix;
+			bool _verbose;
+		};
+
+	public:
+		LLogger(std::ostream& stream = std::cerr);
+
+		~LLogger();
+
+		// thread safe
+		PrintThread Info();
+		PrintThread Err();
+		PrintThread Debug();
+		PrintThread Log(std::string title);
+
+		void EnableLog(std::string title, bool enable);
+		void EnableInfo(bool enable) { _enable_info = enable; }
+		void EnableErr(bool enable) { _enable_err = enable; }
+		void EnableDebug(bool enable) { _enable_debug = enable; }
+		void DisableAllExceptOriginal(bool disable = true) { _enable = !disable; }
+
+		// useless
+		void AddLog(std::string str, bool enable = true);
+		void RemoveLog(std::string str);
+		void RemoveAllLog();
+
+		// not thread safe
+
+
+		void RedirectToFile(std::string filename);
+		void RedirecBack();
 	protected:
-		std::mutex& _mutexPrint;
+		void Redirect(std::ostream& output_stream);
+
+		PrintThread Output(std::string title,bool enable);
+		std::mutex _mutex_stream;
 		std::ostream& _output_stream;
-		std::string _prefix;
-		bool _verbose;
+
+		bool _enable_info;
+		bool _enable_err;
+		bool _enable_debug;
+		bool _enable;
+
+		std::mutex _mutex_map;
+		std::map<std::string, bool> _enable_map;
+
+		std::ofstream _output_file;
+		std::streambuf* _old_buf;
 	};
 
-public:
-	LLogger(std::ostream& stream = std::cerr);
+	extern LLogger lerr;
+	extern LLogger lout;
+	extern LLogger llog;
+}
 
-	~LLogger();
 
-	PrintThread Info();
-	PrintThread Err();
-
-	void EnableInfo(bool enable) { _enable_info = enable; }
-	void EnableErr(bool enable) { _enable_err = enable; }
-	void Redirect(std::ostream& output_stream);
-
-protected:
-	std::mutex _mutex_stream;
-	std::ostream& _output_stream;
-	bool _enable_info;
-	bool _enable_err;
-};
-
-class LoggableObj
-{
-public:
-	LoggableObj() {};
-	virtual ~LoggableObj() {};
-
-	void EnableInfo(bool enable) { _logger.EnableInfo(enable); }
-	void EnableErr(bool enable) { _logger.EnableErr(enable); }
-	void Redirect(std::ostream& output_stream) { _logger.Redirect(output_stream); }
-
-protected:
-	LLogger _logger;
-};
